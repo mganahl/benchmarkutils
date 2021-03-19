3c796f903fcfd393a099915d6fd6af76294993e4
mganahl <martin.ganahl@gmail.com>
2021-03-19 10:40:11 +0000

    add file

---
+++ benchmarkutils/utils.py
@@ -0,0 +1,56 @@
+import cirq
+import os
+import gc
+import pickle
+import tensornetwork as tn
+from tpu_cirq import tpu_simulator
+import numpy as np
+from qsim_circuits.random_circuits import crng_circuit
+from tpu_cirq.tpu_simulator import TPUSimulator
+from circuits import qaoa
+import time
+
+
+def dict_depth(dictionary, current_depth):
+  """
+  Return the depth of the dictionary + current_depth.
+  """
+  if isinstance(dictionary, dict):
+    if len(dictionary.keys()) > 0:
+      return dict_depth(list(dictionary.values())[0], current_depth + 1)
+    return current_depth + 1
+  return current_depth
+
+
+def insert(keys, value, dictionary):
+  """
+  Insert 'value' into a nested dict 'dictionary',
+  using elements of `keys` on the different nesting
+  level.s
+  """
+  assert (dict_depth(dictionary, 0)
+          == len(keys)) or (len(dictionary)
+                            == 0), 'dict depth does not match key length'
+  if len(keys) > 1:
+    if keys[0] not in dictionary:
+      dictionary[keys[0]] = {}
+    insert(keys[1:], value, dictionary[keys[0]])
+    return dictionary
+
+  if keys[0] not in dictionary:
+    dictionary[keys[0]] = []
+
+  dictionary[keys[0]].append(value)
+  return dictionary
+
+
+def record_timing(keys, values, iteration, walltime, dictionary):
+  """
+  Record `values` into `dictionary`.
+  """
+  if iteration == 0:
+    last_key = ['initial']
+  else:
+    last_key = ['subsequent']
+  dict_keys = [f'{k}={v}' for k, v in zip(keys, values)] + last_key
+  return insert(dict_keys, walltime, dictionary)
